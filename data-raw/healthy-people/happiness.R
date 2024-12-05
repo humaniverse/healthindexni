1da0fe7b45821e4de66f6cf68a52822d090b9efb
CatReid <CatrionaReid@redcross.org.uk>
2024-12-05 16:35:28 +0000

    Corrections - happiness
    Corrections - happiness

--- data-raw/healthy-people/happiness.R
+++ data-raw/healthy-people/happiness.R
@@ -1,12 +1,11 @@
 # ---- Load packages ----
 library(tidyverse)
-library(rio)
 
 # ---- Get and clean data ----
 # Happiness
 # Source: https://www.ons.gov.uk/datasets/wellbeing-local-authority/editions/time-series/versions/4
 
-happiness_raw <- import("https://download.ons.gov.uk/downloads/datasets/wellbeing-local-authority/editions/time-series/versions/4.csv")
+happiness_raw <- read_csv("https://download.ons.gov.uk/downloads/datasets/wellbeing-local-authority/editions/time-series/versions/4.csv")
 
 people_happiness <- happiness_raw |>
   filter(
@@ -15,7 +14,7 @@ people_happiness <- happiness_raw |>
     `yyyy-yy` == "2022-23",
     `wellbeing-estimate` == "average-mean"
   ) |>
-  slice(-11) |>
+  filter(`administrative-geography` != "N92000002") |>
   select(
     ltla24_code = `administrative-geography`,
     happiness_score_out_of_10 = `v4_3`,
