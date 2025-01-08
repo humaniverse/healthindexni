c9e39e78327170d24177a0aa52a8a9fd25a4fdf3
CatReid <CatrionaReid@redcross.org.uk>
2025-01-08 14:05:51 +0000

    First draft completed - infant-mortality
    First draft completed - infant-mortality, styled and rendered.
    
    Converted HB to LA.

--- data-raw/healthy-lives/infant-mortality.R
+++ data-raw/healthy-lives/infant-mortality.R
@@ -1,209 +1,54 @@
-# ---- Load ----
+# ---- Load packages ----
 library(tidyverse)
-library(httr)
-library(readxl)
 library(geographr)
-library(sf)
 
-source("R/utils.R")
+# ---- Get and clean data ----
+# Geographical Code Data: Health Board and Local Authority
 
-ni_lookup <-
-  boundaries_lad |>
-  st_drop_geometry() |>
-  filter_codes(lad_code, "^N") |>
-  select(lad_name, lad_code)
+hb_ltla_lookup <- lookup_ltla21_hsct18 |>
+  distinct(ltla21_code, trust18_code)
 
-# ---- Load Latest 3 year birth & death rates ----
-GET(
-  "https://www.nisra.gov.uk/sites/nisra.gov.uk/files/publications/RG_2017.zip",
-  write_disk(tf <- tempfile(fileext = ".zip"))
-)
+hb_lookup <- boundaries_trusts_ni18 |>
+  sf::st_drop_geometry()
 
-unzip(tf, exdir = tempdir())
 
-raw_infant_deaths_2017 <-
-  read_excel(
-    list.files(
-      tempdir(),
-      pattern = "Stillbirths_InfantDeaths_2017.xls",
-      full.names = TRUE
-    ),
-    sheet = "Table 4.4b",
-    range = "A4:C53"
-  )
+# Infant Mortality Data
+# Source: https://data.nisra.gov.uk/
 
-infant_deaths_2017 <-
-  raw_infant_deaths_2017 |>
-  slice(-c(1:6)) |>
-  drop_na() |>
+infant_mortality_raw <- read_csv("https://ws-data.nisra.gov.uk/public/api.restful/PxStat.Data.Cube_API.ReadDataset/INFANTMRHSCT/CSV/1.0/")
+
+infant_mortality_hb <- infant_mortality_raw |>
+  filter(
+    `Grouped Year` == "2018-22",
+    `Health and Social Care Trust` != "NI"
+  ) |>
   select(
-    lad_name = `...1`,
-    infant_mortality_count = `All infant`
-  ) |>
-  mutate(lad_name = str_replace_all(lad_name, "&", "and")) |>
-  left_join(ni_lookup) |>
-  select(lad_code, infant_mortality_count) |>
-  mutate(year = "2017")
-
-raw_live_births_2017 <-
-  read_excel(
-    list.files(
-      tempdir(),
-      pattern = "Births_2017.xls",
-      full.names = TRUE
-    ),
-    sheet = "Table 3.7b",
-    range = "A4:C52"
+    trust18_name = `Health and Social Care Trust`,
+    infant_mortality_per_1k = `VALUE`
   )
 
-live_births_2017 <-
-  raw_live_births_2017 |>
-  slice(-c(1:5)) |>
-  drop_na() |>
+
+# Join datasets
+# Infant mortality data + Trust name and code
+
+infant_mortality_hb <- infant_mortality_hb |>
+  left_join(hb_lookup) |>
+  select(-trust18_name) |>
+  relocate(trust18_code)
+
+
+# Infant mortality data + LA code
+
+lives_infant_mortality <- infant_mortality_hb |>
+  left_join(hb_ltla_lookup) |>
+  mutate(year = "2018-22") |>
   select(
-    lad_name = Area,
-    live_birth_count = `All births`
+    ltla24_code = ltla21_code,
+    infant_mortality_per_1k,
+    year
   ) |>
-  mutate(lad_name = str_replace_all(lad_name, "&", "and")) |>
-  left_join(ni_lookup) |>
-  select(lad_code, live_birth_count) |>
-  mutate(year = "2017")
+  arrange(ltla24_code)
 
-GET(
-  "https://www.nisra.gov.uk/sites/nisra.gov.uk/files/publications/RG_2018.zip",
-  write_disk(tf <- tempfile(fileext = ".zip"))
-)
 
-unzip(tf, exdir = tempdir())
-
-raw_infant_deaths_2018 <-
-  read_excel(
-    list.files(
-      tempdir(),
-      pattern = "Stillbirths_Infant_Deaths_Tables_2018.xlsx",
-      full.names = TRUE
-    ),
-    sheet = "Table 4.4b",
-    range = "A4:C53"
-  )
-
-infant_deaths_2018 <-
-  raw_infant_deaths_2018 |>
-  slice(-c(1:6)) |>
-  drop_na() |>
-  select(
-    lad_name = `...1`,
-    infant_mortality_count = `All infant`
-  ) |>
-  mutate(lad_name = str_replace_all(lad_name, "&", "and")) |>
-  left_join(ni_lookup) |>
-  select(lad_code, infant_mortality_count) |>
-  mutate(year = "2018")
-
-raw_live_births_2018 <-
-  read_excel(
-    list.files(
-      tempdir(),
-      pattern = "Births_Tables_2018.xlsx",
-      full.names = TRUE
-    ),
-    sheet = "Table 3.7b",
-    range = "A3:C51"
-  )
-
-live_births_2018 <-
-  raw_live_births_2018 |>
-  slice(-c(1:5)) |>
-  drop_na() |>
-  select(
-    lad_name = Area,
-    live_birth_count = `All births`
-  ) |>
-  mutate(lad_name = str_replace_all(lad_name, "&", "and")) |>
-  left_join(ni_lookup) |>
-  select(lad_code, live_birth_count) |>
-  mutate(year = "2018")
-
-GET(
-  "https://www.nisra.gov.uk/sites/nisra.gov.uk/files/publications/Stillbirths_InfantDeaths_Tables_2019.xls",
-  write_disk(tf <- tempfile(fileext = ".xls"))
-)
-
-raw_infant_deaths_2019 <-
-  read_excel(
-    tf,
-    sheet = "Table 4.4b",
-    range = "A4:C53"
-  )
-
-infant_deaths_2019 <-
-  raw_infant_deaths_2019 |>
-  slice(-c(1:6)) |>
-  drop_na() |>
-  select(
-    lad_name = `2019`,
-    infant_mortality_count = `All infant`
-  ) |>
-  mutate(lad_name = str_replace_all(lad_name, "&", "and")) |>
-  left_join(ni_lookup) |>
-  select(lad_code, infant_mortality_count) |>
-  mutate(year = "2019")
-
-GET(
-  "https://www.nisra.gov.uk/sites/nisra.gov.uk/files/publications/Births_Tables_2019.xlsx",
-  write_disk(tf <- tempfile(fileext = ".xlsx"))
-)
-
-raw_live_births_2019 <-
-  read_excel(
-    tf,
-    sheet = "Table 3.7b",
-    range = "A3:C51"
-  )
-
-live_births_2019 <-
-  raw_live_births_2019 |>
-  slice(-c(1:5)) |>
-  drop_na() |>
-  select(
-    lad_name = Area,
-    live_birth_count = `All births`
-  ) |>
-  mutate(lad_name = str_replace_all(lad_name, "&", "and")) |>
-  left_join(ni_lookup) |>
-  select(lad_code, live_birth_count) |>
-  mutate(year = "2019")
-
-# ---- Join and calculate mean rate ----
-all_births <-
-  bind_rows(
-    live_births_2017,
-    live_births_2018,
-    live_births_2019
-  )
-
-all_deaths <-
-  bind_rows(
-    infant_deaths_2017,
-    infant_deaths_2018,
-    infant_deaths_2019
-  ) |>
-  mutate(infant_mortality_count = as.double(infant_mortality_count))
-
-joined <-
-  all_births |>
-  left_join(
-    all_deaths,
-    by = c("lad_code", "year")
-  )
-
-infant_mortality <-
-  joined |>
-  mutate(
-    infant_mortality_per_1000 = (infant_mortality_count / live_birth_count) * 1000
-  ) |>
-  group_by(lad_code) |>
-  summarise(infant_mortality_per_1000 = mean(infant_mortality_per_1000))
-
-# Save
-write_rds(infant_mortality, "data/vulnerability/health-inequalities/northern-ireland/healthy-lives/infant-mortality.rds")
+# ---- Save output to data/ folder ----
+usethis::use_data(lives_infant_mortality, overwrite = TRUE)
