3e78eeb272ce58b13adac2886f4ffbf2053cf3ee
CatReid <CatrionaReid@redcross.org.uk>
2025-01-13 13:00:21 +0000

    First draft started - noise-complaints
    First draft started - noise-complaints

--- data-raw/healthy-people/noise-complaints.R
+++ data-raw/healthy-people/noise-complaints.R
@@ -1,3 +1,15 @@
 # Raw data files were not available to download from online.
 # Noise-complaints data was available from a published reported (sourced below).
-#
+# Data is available in the following file path:
+# healthindexni > data-raw > healthy-people > noise-complaints.xlsx
+
+# ---- Load packages ----
+library(tidyverse)
+library(httr)
+library(readxl)
+library(geographr)
+
+# ---- Get and clean data ----
+# Noise Complaints Data
+# Source: https://www.daera-ni.gov.uk/sites/default/files/publications/daera/Noise%20Complaint%20Statistics%20for%20NI%202023-24.PDF
+
