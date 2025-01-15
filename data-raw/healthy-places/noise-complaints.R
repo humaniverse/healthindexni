1f22b32fa48a1a7f58fc9fc79188381b1974c752
CatReid <CatrionaReid@redcross.org.uk>
2025-01-15 15:24:36 +0000

    First draft completed - noise-complaints
    First draft completed - noise-complaints, styled and rendered.

--- data-raw/healthy-places/noise-complaints.R
+++ data-raw/healthy-places/noise-complaints.R
@@ -13,39 +13,39 @@ library(geographr)
 # Noise Complaints Data
 # Source: https://www.daera-ni.gov.uk/sites/default/files/publications/daera/Noise%20Complaint%20Statistics%20for%20NI%202023-24.PDF
 
-noise_complaints_raw <- read_excel("data-raw/healthy-places/noise_complaints_data.xlsx", skip = 4)
+noise_complaints_raw <- read_excel("data-raw/healthy-places/noise_complaints_data.xlsx")
 
 noise_complaints <- noise_complaints_raw |>
-  select(ltla24_name = Council,
-         noise_complaints_per_1k = "Complaints per 1000") |>
+  select(
+    ltla24_name = Council,
+    noise_complaints_per_1k = "Complaints per 1000"
+  ) |>
   mutate(
-    ltla24_name = str_replace_all(ltla24_name, "\\s*City\\b", ""),
+    ltla24_name = str_replace_all(ltla24_name, "\\s*&\\s*", " and "),
     ltla24_name = case_when(
       ltla24_name == "Ards and North Down" ~ "North Down and Ards",
       TRUE ~ ltla24_name
     )
   )
 
-
 # LA ID
 lookup <- lookup_sa11_soa11_lgd18 |>
   select(lgd18_name, lgd18_code) |>
   distinct(lgd18_name, lgd18_code) |>
-  select (ltla24_name = lgd18_name,
-          ltla24_code = lgd18_code)
+  select(
+    ltla24_name = lgd18_name,
+    ltla24_code = lgd18_code
+  )
 
 # Join datasets
 places_noise_complaints <- noise_complaints |>
   left_join(lookup) |>
-  mutate(ltla24_code = if_else(ltla24_name == "Armagh, Banbridge and Craigavon",
-                               "N09000002", ltla24_code))
+  mutate(year = "2023-24") |>
+  select(
+    ltla24_code,
+    noise_complaints_per_1k,
+    year
+  )
 
-
-
-
-
-identical(noise_complaints$ltla24_name, lookup$ltla24_name)
-mismatched_rows <- which(noise_complaints$ltla24_name != lookup$ltla24_name)
-noise_complaints[mismatched_rows, "ltla24_name"]
-lookup[mismatched_rows, "ltla24_name"]
-
+# ---- Save output to data/ folder ----
+usethis::use_data(places_noise_complaints, overwrite = TRUE)
