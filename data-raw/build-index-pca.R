30e05d645d11822177da8768c319f4aae83ed6bb
KarolisKoncevicius <karolis.koncevicius@gmail.com>
2025-04-24 11:35:46 +0300

    updated build-index-pca.R

--- data-raw/build-index-pca.R
+++ data-raw/build-index-pca.R
@@ -1,66 +1,50 @@
 #--- method --------------------------------------------------------------------
 #
-# # Adapted from:
-# # https://www.oecd-ilibrary.org/docserver/9789264043466-en.pdf?expires=1732447217&id=id&accname=guest&checksum=C9FB3C0E94A6DE6D49C811382692119B
+# Strategy adapted from:
+# https://www.oecd-ilibrary.org/docserver/9789264043466-en.pdf?expires=1732447217&id=id&accname=guest&checksum=C9FB3C0E94A6DE6D49C811382692119B
 #
-
 
 #--- libraries -----------------------------------------------------------------
 
 library(tidyverse)
+library(stringi)
 library(psych)
 
 
 #--- prepare indicators --------------------------------------------------------
 
-# load all indicators
-files <- list.files("data", pattern = "^(lives|places|people).*\\.rda", full.names = TRUE)
-indicators <- map(files, ~ get(load(.x)))
-indicators <- map(indicators, ~ select(.x, -starts_with("year")))
+# load
+files      <- list.files("data", pattern = "^(lives|places|people).*\\.rda", full.names = TRUE)
+indicators <- Map(function(x) get(load(x)), files)
 
-# check whether all indicator tables had the same number of columns
-stopifnot(all(lengths(indicators) == 2))
+stopifnot(all(lengths(indicators) == 6))
 
-# NOTE: a few indicators have "lgd14_code" - renaming the column
-# TODO: fix in data-raw
-# TODO: need API key...
-indicators <- map(indicators, ~ rename(.x, any_of(c(ltla24_code = "lgd14_code"))))
+# combine
+# NOTE: a few indicators have "lgd14_code" - will be renaming the column
+indicators <- Map(setNames, indicators, list(c("ltla24_code", "value", "year", "domain", "subdomain", "is_higher_better")))
+indicators <- Map(cbind, indicators, indicator = basename(names(indicators)))
+indicators <- do.call(rbind, indicators)
 
-# combine to a single table
-indicators <- reduce(indicators, left_join, by = "ltla24_code")
-
-names(indicators)[-1] <- str_replace(basename(files), ".rda$", "")
-
-# all the indicators that should be represented as higher = better
-# but are currently higher = worse
-toflip <- c("lives_alcohol_misuse", "lives_childhood_overweight_obesity",
-            "lives_high_blood_pressure", "lives_low_birth_weight", "lives_pupil_absence",
-            "lives_smoking", "lives_teenage_pregnancy",
-            "people_all_mortality", "people_avoidable_deaths", "people_cancer",
-            "people_cardiovascular_conditions", "people_dementia", "people_diabetes",
-            "people_disability_daily_activities", "people_infant_mortality", "people_mental_health",
-            "people_respiratory_conditions", "people_self_harm", "people_suicide",
-            "places_air_pollution", "places_child_poverty", "places_gp_travel_time",
-            "places_household_overcrowding", "places_internet_access",
-            "places_noise_complaints", "places_personal_crime", "places_pharmacy_travel_time",
-            "places_road_safety", "places_sports_centre_travel_time", "places_unemployment"
-            )
-
-# check if all present
-stopifnot(all(toflip %in% colnames(indicators)))
-
-# align the indicators so that higher = better
-indicators[toflip] <- - indicators[toflip]
-
-# scale to z-scores
-indicators[-1] <- map(indicators[-1], function(x) scale(x)[,1])  # NOTE: [,1] to drop attribtes
-
-# impute NA values by using a simple mean
-# note that means are 0 after scaling
-indicators[is.na(indicators)] <- 0
+# clean up
+indicators <- as_tibble(indicators) |>
+  # remove left-over regions from lgd14
+  filter(ltla24_code != "N92000002") |>
+  mutate(indicator = str_replace(indicator, "\\.rda$", "")) |>
+  mutate(subdomain = paste0(domain, "_", str_replace_all(subdomain, " ", "_"))) |>
+  # flip higher = worse to higher = better
+  mutate(value = if_else(is_higher_better, value, -value)) |>
+  select(ltla24_code, indicator, subdomain, value) |>
+  # transform to z-scores
+  group_by(indicator) |>
+  mutate(value = scale(value)[,1]) |>
+  ungroup() |>
+  # impute missing with mean (mean is 0 after z-score transform)
+  mutate(value = if_else(is.na(value), 0, value)) |>
+  group_split(subdomain) |>
+  map(pivot_wider, names_from = indicator, values_from = value)
 
 
-#--- combine -------------------------------------------------------------------
+#--- create subdomains using pca -----------------------------------------------
 
 build_pc_indicators <- function(x) {
   # NOTE: if only one indicator - return it
@@ -97,33 +81,22 @@ build_pc_indicators <- function(x) {
   colSums(t(x) * w)
 }
 
+subdomains <- indicators |>
+  map(~ mutate(.x, pcscore = build_pc_indicators(.x[,-c(1:2)]))) |>
+  map(~ select(.x, ltla24_code, subdomain, pcscore)) |>
+  map(~ rename(.x, !!.x$subdomain[1] := pcscore)) |>
+  map(~ select(.x, -subdomain)) |>
+  reduce(left_join)
+
+
+#--- construct index scores ----------------------------------------------------
+
 quantise <- function(x) {
   findInterval(x, quantile(x, seq(0,1,0.1)), rightmost.closed = TRUE)
 }
 
-# subdomains
-subdomains <- list()
-subdomains$people_difficulties_in_daily_life <- indicators[c("people_disability_daily_activities")]
-subdomains$people_mental_health              <- indicators[c("people_mental_health", "people_suicide", "people_self_harm")]
-subdomains$people_mortality                  <- indicators[c("people_avoidable_deaths", "people_infant_mortality", "people_life_expectancy", "people_all_mortality")]
-subdomains$people_personal_wellbeing         <- indicators[c("people_life_worthwhileness", "people_anxiety", "people_happiness", "people_life_satisfaction")]
-subdomains$people_physical_health_conditions <- indicators[c("people_cancer", "people_cardiovascular_conditions", "people_dementia", "people_diabetes", "people_respiratory_conditions")]
 
-subdomains$lives_behavioural_risk_factors   <- indicators[c("lives_alcohol_misuse", "lives_healthy_eating", "lives_smoking")]
-subdomains$lives_children_and_young_people  <- indicators[c("lives_gsce_attainment", "lives_pupil_absence", "lives_teenage_pregnancy", "lives_young_peoples_training")]
-subdomains$lives_physiological_risk_factors <- indicators[c("lives_high_blood_pressure", "lives_low_birth_weight", "lives_childhood_overweight_obesity")]
-subdomains$lives_protective_measures        <- indicators[c("lives_cancer_screening", "lives_child_vaccine_coverage")]
-
-# subdomains$places_access_to_green_space           <- indicators[c("places_private_outdoor_space")] # NOTE: missing
-subdomains$places_access_to_services              <- indicators[c("places_gp_travel_time", "places_pharmacy_travel_time", "places_sports_centre_travel_time", "places_internet_access")]
-subdomains$places_crime                           <- indicators[c("places_personal_crime")]
-subdomains$places_economic_and_working_conditions <- indicators[c("places_child_poverty", "places_unemployment")]
-subdomains$places_living_conditions               <- indicators[c("places_air_pollution", "places_household_overcrowding", "places_noise_complaints", "places_road_safety")]
-
-subdomains <- data.frame(ltla24_code = indicators$ltla24_code, sapply(subdomains, build_pc_indicators))
-
-
-scores <- data.frame(ltla24_code = indicators$ltla24_code)
+scores <- data.frame(ltla24_code = subdomains$ltla24_code)
 
 # healthy lives
 scores$healthy_lives_score    <- rowSums(select(subdomains, starts_with("lives")))
